0a43a7b6dc66e6defbcaaeea9998ec5d47f8a5d5
shobhitkushwah <shobhitkushwah12345@gmail.com>
2026-06-29 18:49:30 +0530

    ascii

--- lists.py
+++ lists.py
@@ -28,17 +28,24 @@ list1=[]
 # list1.append(1)
 # print(list1)
 # bubble sort 
-list1=[]
-n=int(input("enter the size of list1="))
-for i in range(n):
-    element=int(input(f"enter the element {i+1}="))
-    list1.append(element)
-k=list1[0]
-for i in range(0,len(list1)-1):
-    for j in range(0,len(list1)-1):
-              if list1[j]>list1[j+1]:
-                    temp=list1[j]
-                    list1[j]=list1[j+1]
-                    list1[j+1]=temp 
+# list1=[]
+# n=int(input("enter the size of list1="))
+# for i in range(n):
+#     element=int(input(f"enter the element {i+1}="))
+#     list1.append(element)
+# # k=len(list1)-1
+# count=0
+# for i in range(0,len(list1)-1):
+
+#     for j in range(0,len(list1)-1):
+#               count+=1
+#               if list1[j]>list1[j+1]:
+#                     temp=list1[j]
+#                     list1[j]=list1[j+1]
+#                     list1[j+1]=temp 
+# print(list1)   
+# print(count)
+list1=["d","A","a"]
+list1.sort()
 print(list1)
-            
+print("a">="a")
